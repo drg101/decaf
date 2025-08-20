a3e921952128e82c9f03e3edaf33df4404fd6493
daniel <danielreynolds101@gmail.com>
2025-08-19 22:47:43 -0600

    Update components + bump

--- lib/widgets/add_caffeine_modal.dart
+++ lib/widgets/add_caffeine_modal.dart
@@ -21,131 +21,138 @@ class _AddCaffeineModalState extends ConsumerState<AddCaffeineModal> {
     final caffeineOptions = ref.watch(caffeineOptionsProvider);
 
     return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 10.0, vertical: 16.0),
-      child: ConstrainedBox(
-        constraints: BoxConstraints(
-          maxHeight: MediaQuery.of(context).size.height * 0.9,
-        ),
-        child: SingleChildScrollView(
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                'Add Caffeine',
-                style: Theme.of(context).textTheme.headlineSmall,
-              ),
-              const SizedBox(height: 24),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  IconButton(
-                    icon: const Icon(Icons.remove),
-                    onPressed: () {
-                      setState(() {
-                        _caffeineAmount = (_caffeineAmount - 5).clamp(
-                          0,
-                          double.infinity,
-                        );
-                      });
-                    },
-                  ),
-                  Text(
-                    '${_caffeineAmount.toStringAsFixed(0)} mg',
-                    style: Theme.of(context).textTheme.headlineLarge?.copyWith(
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  IconButton(
-                    icon: const Icon(Icons.add),
-                    onPressed: () {
-                      setState(() {
-                        _caffeineAmount = _caffeineAmount + 5;
-                      });
-                    },
-                  ),
-                ],
-              ),
-              const SizedBox(height: 16),
-              Flexible(
-                child: Wrap(
-                  spacing: 8.0,
+      padding: EdgeInsets.only(
+        bottom: MediaQuery.of(context).viewInsets.bottom,
+      ),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10.0, vertical: 16.0),
+        child: ConstrainedBox(
+          constraints: BoxConstraints(
+            maxHeight: MediaQuery.of(context).size.height * 0.9,
+          ),
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'Add Caffeine',
+                  style: Theme.of(context).textTheme.headlineSmall,
+                ),
+                const SizedBox(height: 24),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    ...caffeineOptions.when(
-                      data: (options) {
-                        return options.where((option) => option.enabled).map((
-                          option,
-                        ) {
-                          final name = '${option.emoji} ${option.name}';
-                          return ChoiceChip(
-                            label: Text(
-                              '$name (${option.caffeineAmount.toStringAsFixed(0)}mg)',
-                            ),
-                            selected: _selectedChipKey == name,
-                            onSelected: (selected) {
-                              setState(() {
-                                if (selected) {
-                                  _selectedChipKey = name;
-                                  _caffeineAmount = option.caffeineAmount;
-                                } else {
-                                  _selectedChipKey = null;
-                                  _caffeineAmount = 0;
-                                }
-                              });
-                            },
+                    IconButton(
+                      icon: const Icon(Icons.remove),
+                      onPressed: () {
+                        setState(() {
+                          _caffeineAmount = (_caffeineAmount - 5).clamp(
+                            0,
+                            double.infinity,
                           );
-                        }).toList();
+                        });
                       },
-                      loading: () => [const CircularProgressIndicator()],
-                      error: (error, stackTrace) => [Text('Error: $error')],
                     ),
-                    ChoiceChip(
-                      label: Text('⚙️ Update Options'),
-                      selected: false,
-                      onSelected: (selected) {
-                        Navigator.of(context).push(
-                          MaterialPageRoute(
-                            builder:
-                                (context) => const ManageCaffeineOptionsPage(),
-                          ),
-                        );
+                    Text(
+                      '${_caffeineAmount.toStringAsFixed(0)} mg',
+                      style: Theme.of(context).textTheme.headlineLarge
+                          ?.copyWith(fontWeight: FontWeight.bold),
+                    ),
+                    IconButton(
+                      icon: const Icon(Icons.add),
+                      onPressed: () {
+                        setState(() {
+                          _caffeineAmount = _caffeineAmount + 5;
+                        });
                       },
                     ),
                   ],
                 ),
-              ),
-              const SizedBox(height: 24),
-              Padding(
-                padding: const EdgeInsets.only(bottom: 16.0),
-                child: ElevatedButton(
-                  onPressed:
-                      _caffeineAmount > 0
-                          ? () {
-                            final selectedDate = ref.read(selectedDateProvider);
-                            final now = DateTime.now();
-                            final timestamp = DateTime(
-                              selectedDate.year,
-                              selectedDate.month,
-                              selectedDate.day,
-                              now.hour,
-                              now.minute,
-                              now.second,
+                const SizedBox(height: 16),
+                Flexible(
+                  child: Wrap(
+                    spacing: 8.0,
+                    children: [
+                      ...caffeineOptions.when(
+                        data: (options) {
+                          return options.where((option) => option.enabled).map((
+                            option,
+                          ) {
+                            final name = '${option.emoji} ${option.name}';
+                            return ChoiceChip(
+                              label: Text(
+                                '$name (${option.caffeineAmount.toStringAsFixed(0)}mg)',
+                              ),
+                              selected: _selectedChipKey == name,
+                              onSelected: (selected) {
+                                setState(() {
+                                  if (selected) {
+                                    _selectedChipKey = name;
+                                    _caffeineAmount = option.caffeineAmount;
+                                  } else {
+                                    _selectedChipKey = null;
+                                    _caffeineAmount = 0;
+                                  }
+                                });
+                              },
                             );
-                            ref
-                                .read(eventsProvider.notifier)
-                                .addEvent(
-                                  EventType.caffeine,
-                                  _selectedChipKey ?? 'Custom Caffeine',
-                                  _caffeineAmount,
-                                  timestamp,
-                                );
-                            Navigator.pop(context);
-                          }
-                          : null,
-                  child: const Text('Add'),
+                          }).toList();
+                        },
+                        loading: () => [const CircularProgressIndicator()],
+                        error: (error, stackTrace) => [Text('Error: $error')],
+                      ),
+                      ChoiceChip(
+                        label: Text('⚙️ Update Options'),
+                        selected: false,
+                        onSelected: (selected) {
+                          Navigator.of(context).push(
+                            MaterialPageRoute(
+                              builder:
+                                  (context) =>
+                                      const ManageCaffeineOptionsPage(),
+                            ),
+                          );
+                        },
+                      ),
+                    ],
+                  ),
                 ),
-              ),
-            ],
+                const SizedBox(height: 24),
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 16.0),
+                  child: ElevatedButton(
+                    onPressed:
+                        _caffeineAmount > 0
+                            ? () {
+                              final selectedDate = ref.read(
+                                selectedDateProvider,
+                              );
+                              final now = DateTime.now();
+                              final timestamp = DateTime(
+                                selectedDate.year,
+                                selectedDate.month,
+                                selectedDate.day,
+                                now.hour,
+                                now.minute,
+                                now.second,
+                              );
+                              ref
+                                  .read(eventsProvider.notifier)
+                                  .addEvent(
+                                    EventType.caffeine,
+                                    _selectedChipKey ?? 'Custom Caffeine',
+                                    _caffeineAmount,
+                                    timestamp,
+                                  );
+                              Navigator.pop(context);
+                            }
+                            : null,
+                    child: const Text('Add'),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
